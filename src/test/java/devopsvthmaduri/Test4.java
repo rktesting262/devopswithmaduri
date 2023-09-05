33b13362fb320c11b3f404fdae2112c12ef65f0e
RAM <RAM@LAPTOP-M9FVOIFN>
2023-09-05 22:50:07 +0530

    comit

---
+++ src/test/java/devopsvthmaduri/Test4.java
@@ -0,0 +1,17 @@
+package devopsvthmaduri;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test4 {
+
+	
+	@Test
+	public void method2()
+	{
+		System.out.println("hi rk");
+		
+		Reporter.log("jenkins");
+	}
+	
+}
